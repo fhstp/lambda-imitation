8127b0e938ac01a1c21eef370c9da1137becd562
Alexander Buchelt <abuchelt@Alexanders-MacBook-Pro.local>
2026-04-29 20:45:28 +0200

    feat: added test files for asc qos tests

---
+++ slrm/cartpole/hyperparams/job_test.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+run_on_gpu() {
+    local gpu_id=$1
+    local urand
+    urand=$(od -vAn -N4 -tu4 < /dev/urandom | tr -d ' ')
+    local seed=$((urand + $$ + gpu_id))
+    CUDA_VISIBLE_DEVICES=$gpu_id ./.conda/envs/lambda/bin/python ./git/lambda-imitation/examples/cartpole_sac_mc.py --wandb --rounds 10 --partial --wandb-project "lambda-imitaiton-cartpole-sanity" --lambda-coef 1 --tau 0.005 --seed $seed
+}
+
+run_on_gpu 0 &
+PID_0=$!
+run_on_gpu 1 &
+PID_1=$!
+
+wait $PID_0; EXIT_0=$?
+wait $PID_1; EXIT_1=$?
+
+if [ $EXIT_0 -ne 0 ] || [ $EXIT_1 -ne 0 ]; then
+    echo "process exit codes: gpu0=$EXIT_0 gpu1=$EXIT_1"
+    exit 1
+fi
